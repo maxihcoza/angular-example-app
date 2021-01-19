ce7c41e6849f29ca96aec38fc5271210f9c90842
maxihcoza <maxiharris93@gmail.com>
2021-01-19 21:16:00 +0200

    view product details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+//maxihcoza - imports for setting up ProductDetailsComponent to display products based on the browser's URL and defined routes
+import { ActivatedRoute } from "@angular/router";
+import { products } from "../products";
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"]
 })
 export class ProductDetailsComponent implements OnInit {
+  //maxihcoza - define properties
+  product;
 
-  constructor() { }
+  //maxihcoza - inject ActivatedRoute into the constructor by adding an argument
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit() {
+    //maxihcoza - first get the product ID from the current route
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+    //maxihcoza - find the product that corresponds with the ID provided in the route
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
-
 }
