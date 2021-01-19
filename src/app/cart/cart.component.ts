be8be29ba38c9a400ab9e9fd2bd5f9d559a88f20
maxihcoza <maxiharris93@gmail.com>
2021-01-19 22:58:44 +0200

    defined the checkout form model

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//maxihcoza - import for providing convenient methods for generating controls
+import { FormBuilder } from '@angular/forms';
 //maxihcoza - import for setting up the cart to display the added products in the cart
 import { CartService } from '../cart.service';
 
@@ -10,10 +12,24 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
   //maxihcoza - define a property to store the products in the cart, which sets the items using the cart service's method
   items = this.cartService.getItems();
+  //maxihcoza - define a propery, gather the user's name and address, then use a method to set the property to a form model containing the name and address fields
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
 
   constructor(
-    private cartService: CartService //maxihcoza - injected cart service
+    private cartService: CartService, //maxihcoza - injected cart service
+    private formBuilder: FormBuilder, //maxihcoza - injected form builder service
   ) { }
+
+  //maxihcoza - define a method to process the form, allowing users to submit their name and address and then resets the form
+  onSubmit(): void {
+    //maxihcoza - process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 
   ngOnInit() {
   }
