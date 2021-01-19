2765d07823b70872db728aefc7c580b18ca70473
maxihcoza <maxiharris93@gmail.com>
2021-01-19 21:31:27 +0200

    used the cart service

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from "@angular/core";
 //maxihcoza - imports for setting up ProductDetailsComponent to display products based on the browser's URL and defined routes
 import { ActivatedRoute } from "@angular/router";
 import { products } from "../products";
+//maxihcoza - import for setting up ProductDetailsComponent to use the cart service to add a product to the cart
+import { CartService } from '../cart.service';
+
 
 @Component({
   selector: "app-product-details",
@@ -12,9 +15,16 @@ export class ProductDetailsComponent implements OnInit {
   //maxihcoza - define properties
   product;
 
+  //maxihcoza - define the method which adds the current product to the cart, which takes the current product as an argument, uses the service's method to add the product to the cart, then displays a message that you've added a product to the cart
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+
   //maxihcoza - inject ActivatedRoute into the constructor by adding an argument
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService //maxihcoza - injected cart service
   ) {}
 
   ngOnInit() {
